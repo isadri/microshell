250ac09f16218e0d120d62e67399e70c572a853c
esm-a <issam.abk01@gmail.com>
2023-05-13 18:45:30 +0100

    micro

--- microshell.c
+++ microshell.c
@@ -9,6 +9,48 @@
 #define SYS_CALL "error: fatal\n"
 #define EXECVE_ERR "error: cannot execute "
 
+typedef struct s_ids
+{
+	pid_t			id;
+	struct s_ids	*next;
+}	t_ids;
+
+t_ids	*last_id(t_ids *ids)
+{
+	while (ids && ids->next)
+		ids = ids->next;
+	return (ids);
+}
+
+void	add_id(t_ids **ids, pid_t id)
+{
+	t_ids	*tmp;
+
+	if (*ids == NULL)
+	{
+		*ids = malloc(sizeof(t_ids));
+		(*ids)->id = id;
+		(*ids)->next = NULL;
+		return ;
+	}
+	tmp = NULL;
+	add_id(&tmp, id);
+	last_id(*ids)->next = tmp;
+}
+
+void	clear(t_ids **ids)
+{
+	t_ids	*tmp;
+
+	while (*ids)
+	{
+		tmp = *ids;
+		*ids = (*ids)->next;
+		free(tmp);
+	}
+	*ids = NULL;
+}
+
 size_t	ft_strlen(char *str)
 {
 	size_t	len;
@@ -25,10 +67,7 @@ int	ft_fork(void)
 
 	id = fork();
 	if (id == -1)
-	{
-		// write(2, SYS_CALL, 14);
 		exit(1);
-	}
 	return (id);
 }
 
@@ -70,45 +109,29 @@ int	set_arguments(char **args, char **av)
 	return (av[i] && !strcmp(av[i], "|") ? i + 1 : i);
 }
 
-void	execute_one_cmd(char **args, char **env)
+void	execute_cd_cmd(char **args)
 {
-	if (strcmp(args[0], "cd") == 0)
+	if (args[1] == NULL || args[2])
 	{
-		if (args[1] == NULL || args[2])
-		{
-			write(2, CD_NUM, 26);
-			return ;
-		}
-		if (chdir(args[1]) == -1)
-		{
-			write(2, CD_FAIL, 26);
-			write(2, args[1], ft_strlen(args[1]));
-			write(2, "\n", 1);
-			return ;
-		}
+		write(2, CD_NUM, 26);
+		return;
 	}
-	else if (ft_fork() == 0)
+	if (chdir(args[1]) == -1)
 	{
+		write(2, CD_FAIL, 26);
+		write(2, args[1], ft_strlen(args[1]));
+		write(2, "\n", 1);
+		return;
+	}
+}
+
+void	execute_one_cmd(char **args, char **env, int stdin_tmp)
+{
+	if (fork() == 0)
+	{
+		close(stdin_tmp);
 		execve(args[0], args, env);
 		print_error(args[0]);
-	}
-}
-
-void	ft_pipe(int *fd)
-{
-	if (pipe(fd) == -1)
-	{
-		// write(2, SYS_CALL, 14);
-		exit(1);
-	}
-}
-
-void	ft_dup2(int fd1, int fd2)
-{
-	if (dup2(fd1, fd2) == -1)
-	{
-		// write(2, SYS_CALL, 14);
-		exit(1);
 	}
 }
 
@@ -117,28 +140,15 @@ void	redirect_io(int fd[2], int pipe_nbr, int cmd_nbr)
 	if (pipe_nbr == 0)
 		return ;
 	if (cmd_nbr < pipe_nbr)
-		ft_dup2(fd[1], 1);
+		dup2(fd[1], 1);
 	close(fd[0]);
 	close(fd[1]);
-}
-
-int	count_all(char **av)
-{
-	int 	a = 0;
-	int		i = 0;
-
-	while (av[i])
-	{
-		if (strcmp(av[i], "|") == 0)
-			++a;
-		i++;
-	}
-	return (a);
 }
 
 int	main(int ac, char **av, char **env)
 {
 	char	*args[10000];
+	t_ids	*ids;
 	int		stdin_tmp;
 	int		pipe_nbr;
 	int		args_nbr;
@@ -148,79 +158,69 @@ int	main(int ac, char **av, char **env)
 	int		i;
 
 	if (ac < 2)
-		exit(1);
+		exit(0);
 	for (int f = 0; f < 10000; f++)
 		args[f] = NULL;
 	++av;
+	stdin_tmp = dup(0);
 	i = -1;
 	while (++i < ac)
 	{
-		stdin_tmp = dup(0);
-		if (stdin_tmp == -1)
-		{
-			write(2, SYS_CALL, 14);
-			exit(1);
-		}
 		pipe_nbr = count_pipes(av + i);
 		if (pipe_nbr == 0)
 		{
 			args_nbr = set_arguments(args, av + i);
 			if (args_nbr != 0)
-				execute_one_cmd(args, env);
+			{
+				if (strcmp(args[0], "cd") == 0)
+					execute_cd_cmd(args);
+				else
+				{
+					execute_one_cmd(args, env, stdin_tmp);
+					wait(NULL);
+				}
+			}
 			i += args_nbr;
 			continue ;
 		}
+		ids = NULL;
 		cmd_nbr = 0;
 		id = 0;
 		while (i < ac && av[i] && strcmp(av[i], ";"))
 		{
-			ft_pipe(fd);
+			pipe(fd);
 			args_nbr = set_arguments(args, av + i);
 			if (strcmp(args[0], "cd") == 0)
 			{
-				if (args[1] == NULL || args[2])
-					write(2, CD_NUM, 26);
-				else if (chdir(args[1]) == -1)
-				{
-					write(2, CD_FAIL, 26);
-					write(2, args[1], 4096);
-					write(2, "\n", 1);
-				}
+				execute_cd_cmd(args);
 				i += args_nbr;
-				if (pipe_nbr)
-				{
-					close(fd[0]);
-					close(fd[1]);
-				}
+				dup2(fd[0], 0);
+				close(fd[0]);
+				close(fd[1]);
 				continue;
 			}
 			id = ft_fork();
 			if (id == 0)
 			{
+				clear(&ids);
 				redirect_io(fd, pipe_nbr, cmd_nbr);
+				close(stdin_tmp);
 				execve(args[0], args, env);
 				print_error(args[0]);
 			}
-			if (pipe_nbr)
-			{
-				ft_dup2(fd[0], 0);
-				close(fd[0]);
-				close(fd[1]);
-				wait(NULL);
-			}
+			add_id(&ids, id);
+			dup2(fd[0], 0);
+			close(fd[0]);
+			close(fd[1]);
 			cmd_nbr++;
 			i += args_nbr;
 		}
-		ft_dup2(stdin_tmp, 0);
-		close(stdin_tmp);
-		// if (id != 0)
-		// 	waitpid(id, NULL, 0);
-		// wait(NULL);
-	// while (pipe_nbr--)
-	// 	wait(NULL);
+		dup2(stdin_tmp, 0);
+		stdin_tmp = -1;
+		for (t_ids *tmp = ids; tmp; tmp = tmp->next)
+			waitpid(tmp->id, NULL, 0);
+		clear(&ids);
 	}
-	int count = count_all(av);
-	while (count--)
-		wait(NULL);
+	close(stdin_tmp);
 	return (0);
 }
